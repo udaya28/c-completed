24587c20439332d7d08ce47f9ac736c85cc71800
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-03-24 12:12:47 +0530

    Create ass1_3.c

---
+++ ass1_3.c
@@ -0,0 +1,38 @@
+/*3.	Mansi is fond of solving mental ability questions. 
+Today while solving some questions she came across a series which 
+she was unable to solve. Help her to find the Nth term of the series.
+N  Nth term
+1  3
+2  8
+3  18
+4  32
+5  60
+.
+.
+.
+10  300
+Input:
+The first line of input contains an integer T denoting 
+the number of test cases. For each test case, there is an integer N.
+Output:
+For each test case, print the Nth term of the series.
+Constraints:
+1<=T<=200
+1 <= N <=6000
+Example:
+Input:
+6
+1
+2
+3
+4
+5
+10
+Output:
+3
+8
+18
+32
+60
+300
+*/
